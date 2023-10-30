11cbb22b723ec7fa0f5d50ce15cf065283f91a6b
hackerbhind <144337511+hackerbhind@users.noreply.github.com>
2023-10-30 12:22:03 +0530

    Bringup for pixel experience plus

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_gta4l.mk
+    $(LOCAL_DIR)/aosp_gta4l.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_gta4l-user \
-    lineage_gta4l-userdebug \
-    lineage_gta4l-eng
+    aosp_gta4l-user \
+    aosp_gta4l-userdebug \
+    aosp_gta4l-eng
